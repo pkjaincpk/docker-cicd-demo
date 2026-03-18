d3fdbcc65ddff3b33d7ace7253edc2a958fadb81
pkjaincpk <pkjaincpk@gmail.com>
2026-03-18 11:50:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "CI/CD is working from Ubuntu VM 🚀"
+    return "CI/CD is working from Ubuntu VM Automatically 🚀"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
